b10c6880a6db022f76f7a73a64ba6829df8c4ded
Tiago Barbosa <tiago@farias.cc>
2021-11-11 21:38:18 -0300

    Initial commit - Flutter and Dart studies, creating app MyBank

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mybank
+
+> Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+With the object for studies of Dart and Flutter.
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Column(
+        children: [
+          Text(
+            'Bem vindo ao MyBank!',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Craft beautiful UIs',
+            textDirection: TextDirection.ltr,
+          ),
+          Column(
+            children: [
+              Text(
+                'Craft beautiful apps',
+                textDirection: TextDirection.ltr,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
