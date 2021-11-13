b863f1d418b5992c289f1cc9085c13bd937b4857
Tiago Barbosa <tiago@farias.cc>
2021-11-13 19:25:10 -0300

    Layout with widgets, forms and code refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,132 @@
+// ignore_for_file: unnecessary_null_comparison
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
+void main() => runApp(const MyBankApp());
+
+class MyBankApp extends StatelessWidget {
+  const MyBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controllerAccountNumber =
+      TextEditingController();
+  final TextEditingController _controllerValue = TextEditingController();
+
+  FormularioTransferencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Criando Transferência"),
+      ),
+      body: Column(
         children: [
-          Text(
-            'Bem vindo ao MyBank!',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Craft beautiful UIs',
-            textDirection: TextDirection.ltr,
-          ),
-          Column(
-            children: [
-              Text(
-                'Craft beautiful apps',
-                textDirection: TextDirection.ltr,
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
               ),
-            ],
-          )
+              decoration: InputDecoration(
+                  icon: Icon(Icons.account_box),
+                  labelText: "Número da conta",
+                  hintText: '0000'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on_outlined),
+                  labelText: "Valor",
+                  hintText: '0.00'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint("clicou no botão");
+              final int? numeroConta =
+                  int.tryParse(_controllerAccountNumber.text);
+              final double? valor = double.tryParse(_controllerValue.text);
+              if (numeroConta != null && valor != null) {
+                final transferenciaCriada = Transferencia(valor, numeroConta);
+                debugPrint("$transferenciaCriada");
+              }
+            },
+            child: const Text("Confirmar"),
+          ),
         ],
       ),
     );
+  }
+}
+
+class ListaTransferencia extends StatelessWidget {
+  const ListaTransferencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Transferências"),
+      ),
+      body: Column(children: [
+        ItemTransferencia(Transferencia(100.00, 1000)),
+        ItemTransferencia(Transferencia(200.00, 2000)),
+        ItemTransferencia(Transferencia(300.00, 3000)),
+        ItemTransferencia(Transferencia(400.00, 4000)),
+        ItemTransferencia(Transferencia(500.55, 5000))
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+  // ignore: use_key_in_widget_constructors
+  const ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.monetization_on_outlined),
+        title: Text("Valor transferido: ${_transferencia.valor.toString()}"),
+        subtitle:
+            Text("Conta Corrente: ${_transferencia.numeroConta.toString()}"),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// ignore: unused_import
 import 'package:mybank/main.dart';
 
 void main() {
